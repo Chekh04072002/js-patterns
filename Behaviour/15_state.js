9c4347a7f638bc95a62bc1efeb60e815c06b26d9
Sasha <chekh-sasha3@mail.ru>
2022-07-08 03:25:15 +0300

    added state

---
+++ Behaviour/15_state.js
@@ -0,0 +1,71 @@
+// Можем создавать классы и можем делегировать изменение состояние этих классов на общий класс
+
+class Light {
+    constructor(light) {
+        this.light = light;
+    }
+}
+
+class RedLight extends Light{
+    constructor() {
+        super('red');
+    }
+    sign() {
+        return 'Стой';
+    }
+}
+
+class YellowLight extends Light{
+    constructor() {
+        super('yellow');
+    }
+    sign() {
+        return 'Внимание';
+    }
+}
+
+class GreenLight extends Light{
+    constructor() {
+        super('green');
+    }
+    sign() {
+        return 'Едь';
+    }
+}
+
+class TrafficLight {
+    constructor() {
+        this.states = [
+            new RedLight(),
+            new YellowLight(),
+            new GreenLight()
+        ];
+        this.current = this.states[0];
+    }
+    change() {
+        const total = this.states.length;
+        let index = this.states.findIndex(light => light === this.current);
+
+        if (index + 1 < total) {
+            this.current = this.states[index + 1];
+        }
+        else {
+            this.current = this.states[0];
+        }
+    }
+    sign() {
+        return this.current.sign()
+    }
+}
+
+const traffic = new TrafficLight();
+console.log(traffic.sign());
+traffic.change();
+
+console.log(traffic.sign());
+traffic.change();
+
+console.log(traffic.sign());
+traffic.change();
+
+console.log(traffic.sign());
